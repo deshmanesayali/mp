76a5154425dc8d30a2f87d57b191cd1afb98b290
Sayali Deshmane <sd796@njit.edu>
2018-10-06 16:37:16 -0700

    Task: added classes main, csv, html and system

--- public/index.php
+++ public/index.php
@@ -5,5 +5,26 @@
  * Date: 10/6/2018
  * Time: 1:57 PM
  */
-echo 'test';
-echo 'hi';
+main:: start("csv_project.csv");
+class main
+{
+    static public function start(){
+
+    }
+}
+class csv{
+    static public function getRecords(){
+
+    }
+}
+class html{
+    static public function generateTable(){
+
+    }
+}
+
+class system{
+    static public function Printpage(){
+        
+    }
+}
