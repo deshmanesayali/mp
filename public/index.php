90296633f92f287486a42275ddd3d5088b42af36
Sayali Deshmane <sd796@njit.edu>
2018-10-06 16:54:10 -0700

    Task: added code to html

--- public/index.php
+++ public/index.php
@@ -43,26 +43,51 @@ class recordFactory{
 
 class html
 {
-    static public function generateTable($records)
+
+    public static function createTable($array)
     {
-        $count = 0;
-        foreach ($records as $record) {
-            if ($count == 0) {
-                $array = $record->ReturnArray();
-                $fields = array_keys($array);
-                $values = array_values($array);
-                print_r($fields);
-                print_r($values);
-            } else {
-                $array = $record->ReturnArray();
-                $value = array_values($array);
-                print_r($value);
-            }
-            $count++;
+
+
+        echo "
+           <html lang=\"en\">
+           <head>
+           <meta charset=\"utf-8\">
+           <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
+           <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\">
+           <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js\"></script>
+           <script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\"></script>
+           </head>
+           </html>";
+
+        //table row
+        $html = '<table class="table table-striped">';
+        // header row
+        $html .= '<tr>';
+        foreach($array[0] as $key=>$value){
+            $html .= '<th>' . htmlspecialchars($key) . '</th>';
         }
-        return $records;
+        $html .= '</tr>';
+
+        // data rows
+        foreach( $array as $key=>$value){
+            $html .= '<tr>';
+            foreach($value as $key2=>$value2){
+                $html .= '<td>' . htmlspecialchars($value2) . '</td>';
+            }
+            $html .= '</tr>';
+        }
+
+        // finish table and return it
+
+        $html .= '</table>';
+        return $html;
+
+
+
     }
+
 }
+
 
 class system{
     static public function Printpage(){
